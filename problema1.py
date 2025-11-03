5a86f21d3a1dcecdaf36dd79c160ebcfbac4c7ce
Yoseph-Andy <221949@unsaac.edu.pe>
2025-11-03 11:50:05 -0500

    Se actualiza el texto 1

--- problema1.py
+++ problema1.py
@@ -1,8 +1,1 @@
-casos = int(input())  
-
-for i in range(casos):
-    numeros = list(map(int, input().split()))
-    if sum(numeros) // len(numeros) == numeros[0]:
-        print("YES")
-    else:
-        print("NO")
+print("Hello, World!")
